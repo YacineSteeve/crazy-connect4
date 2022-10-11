027864fd4aa98a57ca9309d8f95484774fc558e3
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-10-11 19:29:09 +0200

    feat(game): Diagonals four-in-row checking.

--- src/game.py
+++ src/game.py
@@ -28,20 +28,64 @@ MODE = {
 WIN_PATTERNS = ['0000', '1111']
 
 
-def four_in(seq: List[int]) -> Union[bool, Tuple[bool, str, int, int]]:
+def four_in(seq: List[int]) -> Union[bool, Tuple[bool, str, str]]:
     joined_row = ''.join(map(str, seq))
 
     for pattern in WIN_PATTERNS:
         result = re.findall(pattern, joined_row)
         if result:
             position = joined_row.index(result[0])
-            return True, result[0], position, position + 3
+            return True, result[0], joined_row
 
     return False
 
 
-def find_four(settings=None) -> Union[bool, Tuple[bool, int]]:
-    settings = SETTINGS
+def min_four_diags(matrix: List[List[int]]) -> List[List[int]]:
+    n, m = len(matrix), len(matrix[0])
+    diags = []
+
+    if m >= 4:
+        for col in range(0, m - 3):
+            diag = []
+            row = 0
+            while row < n and col < m:
+                diag.append(matrix[row][col])
+                row += 1
+                col += 1
+            diags.append(diag)
+
+        for col in range(3, m):
+            diag = []
+            row = 0
+            while row < n and col >= 0:
+                diag.append(matrix[row][col])
+                row += 1
+                col -= 1
+            diags.append(diag)
+
+    if n >= 5:
+        for row in range(1, n - 4):
+            diag = []
+            col = m - 1
+            while row < n and col >= 0:
+                diag.append(matrix[row][col])
+                row += 1
+                col -= 1
+            diags.append(diag)
+
+        for row in range(1, n - 4):
+            diag = []
+            col = 0
+            while row < n and col < m:
+                diag.append(matrix[row][col])
+                row += 1
+                col += 1
+            diags.append(diag)
+
+    return diags
+
+
+def find_four() -> Union[bool, Tuple[bool, int]]:
     reduced_matrix = list(filter(lambda r: any(map(lambda x: x != EMPTY, r)), BOXES_MATRIX))
 
     for row in reduced_matrix:
@@ -55,6 +99,12 @@ def find_four(settings=None) -> Union[bool, Tuple[bool, int]]:
             res = four_in(col)
             if res:
                 logger.debug(f'Col : {res}')
+                break
+
+        for diag in min_four_diags(reduced_matrix):
+            res = four_in(diag)
+            if res:
+                logger.debug(f'Diag : {res}')
                 break
 
     return False
