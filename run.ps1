3bf622ae5cf5c9eb1d9a14f2e2e2750bdc15a737
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-10-27 16:48:00 +0200

    docs(comments): add comments to the script files

--- run.ps1
+++ run.ps1
@@ -1,24 +1,39 @@
 #! /usr/bin/pwsh
 
+# This script is an all-in-one that helps to setup, run and rerun the app on Windows.
+
 if ($(python3 --version) | Select-String ".*3.10.*") {
+    # Python version needed to run the app is 3.10
+    # (lower or higher not supported)
+
     if (Test-Path -Path venv) {
+        # A virtual environment exists (that is to say that the app has already been installed).
         Clear-Host
-        venv\Scripts\python main.py
+        venv\Scripts\python main.py  # Launch the app, assuming the virtualenv is active.
     } else {
         python3 -m venv venv
         venv\Scripts\activate
         if (venv\Scripts\pip3 install -r requirements.txt) {
             $VLC_IS_OK = "False"
             if (powershell.exe -ExecutionPolicy Bypass .\Deploy-VLCMediaPlayer.ps1 -DeploymentType "Install" -DeployMode "NonInteractive") {
+                # Install VLC MediaPlayer, used to play background music in the app
                 $VLC_IS_OK = "True"
             }
+
+            # --- The .env file will be used in the program to check if vlc is installed ---
             New-Item .env
             "VLC_IS_OK=$VLC_IS_OK" >> .env
+            # ------------------------------------------------------------------------------
+
+            # --- Run tests and clear the generated files ---
             pytest --cov=src --cache-clear --verbosity=1
             Remove-Item -Recurse .\.pytest_cache
             Remove-Item .coverage
+            # -------------------------------------------
+
             Write-Host "`nCrazy Connect 4 have been installed successfully" -ForegroundColor green
             Write-Host "Thank for your trust and enjoy ! `n" -ForegroundColor green
+
             venv\Scripts\python main.py
         }
     }
--- run.sh
+++ run.sh
@@ -1,36 +1,50 @@
 #!/usr/bin/sh
 
+# This script is an all-in-one that helps to setup, run and rerun the app on Linux and MacOS.
+
 if [ $(python3 --version | grep -c '3.10') -eq 1 ];
+# Python version needed to run the app is 3.10
+# (lower or higher not supported)
 then
-  if [ -d venv ];
-  then
-    clear;
-    venv/bin/python3 main.py;
-  else
-    if sudo apt install -y python3.10-venv;
+    if [ -d venv ];
+    # A virtual environment exists (that is to say that the app has already been installed).
     then
-      python3 -m venv venv;
-      . venv/bin/activate;
-      if venv/bin/pip3 install -r requirements.txt;
-      then
-        VLC_IS_OK=False
-        if sudo apt-get install -y vlc;
+        clear;
+        venv/bin/python3 main.py; # Launch the app, assuming the virtualenv is active.
+    else
+        if sudo apt install -y python3.10-venv;
         then
-            VLC_IS_OK=True;
+            python3 -m venv venv;
+            . venv/bin/activate;
+            if venv/bin/pip3 install -r requirements.txt;
+            then
+                VLC_IS_OK=False
+                if sudo apt-get install -y vlc;
+                # Install the vlc package for Linux, used to play background music in the app
+                then
+                    VLC_IS_OK=True;
+                fi
+
+                # --- The .env file will be used in the program to check if vlc is installed ---
+                touch .env
+                echo "VLC_IS_OK=$VLC_IS_OK" >> .env
+                # ------------------------------------------------------------------------------
+
+                # --- Run tests and clear the generated files ---
+                pytest --cov=src --cache-clear --verbosity=1;
+                rm -rf __pycache__;
+                rm -rf ./.pytest_cache;
+                rm .coverage;
+                # -------------------------------------------
+
+                printf '\n\t\033[0;32m Crazy Connect 4 have been installed successfully !'
+                printf 'Thanks for your trust and enjoy ! \033[0m\n\n';
+
+                venv/bin/python3 main.py;
+            fi
         fi
-        touch .env
-        echo "VLC_IS_OK=$VLC_IS_OK" >> .env
-        pytest --cov=src --cache-clear --verbosity=1;
-        rm -rf __pycache__;
-        rm -rf ./.pytest_cache;
-        rm .coverage;
-        printf '\n\t\033[0;32m Crazy Connect 4 have been installed successfully !'
-        printf 'Thanks for your trust and enjoy ! \033[0m\n\n';
-        venv/bin/python3 main.py;
-      fi
     fi
-  fi
 else
-  printf '\n\t\033[0;31mInstallation failed ! Your Python version is too old to run the app.';
-  printf '\n\tVersion found: %s , required: >= 3.10 \033[0m' "$(python3 --version)";
+    printf '\n\t\033[0;31mInstallation failed ! Your Python version is too old to run the app.';
+    printf '\n\tVersion found: %s , required: >= 3.10 \033[0m' "$(python3 --version)";
 fi
