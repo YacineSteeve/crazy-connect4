0e4037dbabfae540759334053402aefc9a243f64
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-10-29 17:13:24 +0200

    docs: add comments and docstrings

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ if __name__ == '__main__':
         logger.debug("Exit App")
         sys.exit()
 
+    # Initialize game settings
     settings = config.Settings()
     settings.save()
 
     popup = GameModePopUp()
     popup.show()
 
+    # Generate a file to save the game stats.
     with open('games.csv', 'w') as file:
         head = ','.join(f'Move{i}' for i in range(1, settings.get_number_of_boxes + 1))
         head += ',Winner\n'
         file.write(head)
 
+    # Launch the game after the popup window is closed
     game.init()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+"""This module provides some utilities functions used in the program."""
+
 import random
 import os
 import socket
@@ -8,19 +10,45 @@ Coords = Tuple[int, int]
 Matrix = List[List[int]]
 
 TIME = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-USER_INFO = f'{os.getlogin()}.{socket.gethostbyname(socket.gethostname())}'
+USER_INFO = f'{os.getlogin()}_{socket.gethostbyname(socket.gethostname())}'    # user_ip-address
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-def random_column(columns_number):
+def random_column(columns_number: int) -> int:
+    """This function returns a random number between 0 and the board columns number.
+
+    Args:
+        columns_number (int): The number of columns in the game board.
+
+    Returns:
+        int: a random integer between 0 and columns_number.
+
+    """
     return random.randint(0, columns_number - 1)
 
 
 def middle(point_1: Coords, point_2: Coords) -> Coords:
+    """This function computes and returns the coordinates of the middle of a section, represented by its two endpoints.
+
+    Args:
+        point_1 (Tuple[int, int]): The coordinates of the first endpoint of the section.
+        point_2 (Tuple[int, int]): The coordinates of the second endpoint of the section.
+
+    Returns:
+        Tuple[int, int]: The coordinates of the middle point of the section.
+
+    """
     return (point_2[0] + point_1[0]) // 2, (point_2[1] + point_1[1]) // 2
 
 
 def transpose(matrix: Matrix) -> Matrix:
-    n, m = len(matrix), len(matrix[0])
+    """This function computes the transpose of a given 2D matrix.
 
-    return [[matrix[i][j] for i in range(n)] for j in range(m)]
+    Args:
+        matrix (List[List[int]]): A 2D matrix of integers.
+
+    Returns:
+        List[List[int]]: The transpose of matrix.
+
+    """
+    return [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
