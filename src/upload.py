df8875e9aa8a72eda00be72c22b6fdd79e114518
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-10-30 22:59:02 +0100

    docs: add comments and docstrings

--- src/upload.py
+++ src/upload.py
@@ -16,6 +16,15 @@ STORE_PARAMS = {
 
 
 def upload_file(file_type: Literal['logs', 'games']) -> str:
+    """This function send logs file and game stats file to be stored on https://dev.filestack.com/ .
+
+    Args:
+        file_type (str): Tells whether the logs or the game stats are to be uploaded.
+
+    Returns:
+        str: The url to the uploaded file.
+
+    """
     if file_type == 'logs':
         file = LOG_FILE.replace(USER_INFO, f'{USER_INFO}_{TIME}')
         os.rename(LOG_FILE, file)
